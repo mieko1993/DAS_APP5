7d92a433a074e02f1dbb8c448cd4c3db4af94e20
mieko nagatome <mieko_193@hotmail.com>
2015-08-06 16:51:32 -0500

    settins with all activities

--- app/src/main/java/com/team16/antenna/trend/drivingassistancesystem/SettingsAlarms.java
+++ app/src/main/java/com/team16/antenna/trend/drivingassistancesystem/SettingsAlarms.java
@@ -1,38 +1,31 @@
 package com.team16.antenna.trend.drivingassistancesystem;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 
-public class SettingsAlarms extends ActionBarActivity {
+public class SettingsAlarms extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_alarms);
+        super.onCreateDrawer();
+        //Action bar needs to be instatiate in every Activity that has layout
+        setUpActionBar();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_settings_alarms, menu);
-        return true;
+    //Set up Action Bar
+    private void setUpActionBar() {
+        //Uses superclass method to instantiate ActionBar
+        mActionBarToolbar = getActionBarToolbar();
+        mActionBarToolbar.setTitle(getResources().getString(R.string.title_activity_settings_alarms));
+        setSupportActionBar(mActionBarToolbar);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    //when button Save is clicked
+    public void onClickButtonSaveSettingsAlarms(View view){
+        super.onBackPressed();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/team16/antenna/trend/drivingassistancesystem/SettingsUnits.java
+++ app/src/main/java/com/team16/antenna/trend/drivingassistancesystem/SettingsUnits.java
@@ -1,38 +1,31 @@
 package com.team16.antenna.trend.drivingassistancesystem;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
 
-public class SettingsUnits extends ActionBarActivity {
+public class SettingsUnits extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_units);
+        super.onCreateDrawer();
+        //Action bar needs to be instatiate in every Activity that has layout
+        setUpActionBar();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_settings_units, menu);
-        return true;
+    //Set up Action Bar
+    private void setUpActionBar() {
+        //Uses superclass method to instantiate ActionBar
+        mActionBarToolbar = getActionBarToolbar();
+        mActionBarToolbar.setTitle(getResources().getString(R.string.title_activity_settings_units));
+        setSupportActionBar(mActionBarToolbar);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    //when button Save is clicked
+    public void onClickButtonSaveSettingsUnits(View view){
+        super.onBackPressed();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
