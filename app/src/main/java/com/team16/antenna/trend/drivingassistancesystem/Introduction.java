54e2387c0fffad6ef9547a968f927bbbe6af2530
mieko nagatome <mieko_193@hotmail.com>
2015-08-06 12:49:30 -0500

    Initial commit

---
+++ app/src/main/java/com/team16/antenna/trend/drivingassistancesystem/Introduction.java
@@ -0,0 +1,74 @@
+package com.team16.antenna.trend.drivingassistancesystem;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+
+public class Introduction extends ActionBarActivity {
+
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        setContentView(R.layout.activity_introduction);
+
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(adapter);
+    }
+
+    public void onClickButtonOBDConnection(View view){
+        Intent i=new Intent(Introduction.this,OBD.class);
+        startActivity(i);
+    }
+
+    public class PagerAdapter extends FragmentPagerAdapter {
+
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public Fragment getItem(int arg0) {
+            switch (arg0) {
+                case 0:
+                    return new Intro1();
+                case 1:
+                    return new Intro2();
+                case 2:
+                    return new Intro3();
+                case 3:
+                    return new Intro4();
+                case 4:
+                    onDestroy();
+                default:
+                    return null;
+            }
+        }
+
+        public int getCount() {
+            return 4;
+        }
+
+        public CharSequence getPageTitle(int position) {
+            String title = null;
+            switch (position) {
+                case 0:
+                    title = "1";
+                    break;
+                case 1:
+                    title = "2";
+                    break;
+                case 2:
+                    title = "3";
+                    break;
+                case 3:
+                    title = "4";
+                    break;
+            }
+            return title;
+        }
+    }
+}
---
+++ app/src/main/java/com/team16/antenna/trend/drivingassistancesystem/Terms.java
@@ -0,0 +1,15 @@
+package com.team16.antenna.trend.drivingassistancesystem;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class Terms extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_terms);
+    }
+
+}
